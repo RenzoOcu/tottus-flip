6baeb7a083d7929477add5167b29532e908ef1bf
unknown <renzoocupaaliaga@gmail.com>
2024-11-25 22:50:02 -0500

    fix: errors  on build

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,11 @@
-import { PrismaClient } from "@prisma/client"
- 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
- 
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined };
+
+// Usa una instancia única de PrismaClient en desarrollo o crea una nueva en producción.
 export const db = globalForPrisma.prisma || new PrismaClient();
- 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = db;
+}
+
